8f8e8ce8c5f948ee71649a26474f7e73596ccf4a
Quinn Kaji <quinnkaji@gmail.com>
2023-09-13 08:47:28 -1000

    Revert "Made a checkerboard of red and black tiles"
    This reverts commit 73fcee59bd258ab42ad425f6c87c87fecbf76ff5.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const container = document.createElement("div");
 document.body.append(container);
 container.style.width = "400px";
@@ -6,33 +7,35 @@ container.style.display = "flex";
 container.style.flexWrap = "wrap";
 
 for (let i = 0; i < 64; i++) {
-  const randomRgbColor = () => {
-    let r = Math.floor(Math.random() * 256); // Random between 0-255
-    let g = Math.floor(Math.random() * 256); // Random between 0-255
-    let b = Math.floor(Math.random() * 256); // Random between 0-255
-    return "rgb(" + r + "," + g + "," + b + ")";
-  };
+  // const randomRgbColor = () => {
+  //   let r = Math.floor(Math.random() * 256); // Random between 0-255
+  //   let g = Math.floor(Math.random() * 256); // Random between 0-255
+  //   let b = Math.floor(Math.random() * 256); // Random between 0-255
+  //   return "rgb(" + r + "," + g + "," + b + ")";
+  // };
   const tile = document.createElement("div");
   tile.style.width = "12.5%";
   tile.style.height = "12.5%";
-  tile.style.background = randomRgbColor();
+  tile.style.background = "red" ;
   container.append(tile);
 
-  //   if (i % 2 === 1 && i < 8) {
-  //     tile.style.background = "black";
-  //   } else if (i % 2 === 0 && i >= 8 && i < 16) {
-  //     tile.style.background = "black";
-  //   } else if (i % 2 === 1 && i >= 16 && i < 24) {
-  //     tile.style.background = "black";
-  //   } else if (i % 2 === 0 && i >= 24 && i < 32) {
-  //     tile.style.background = "black";
-  //   } else if (i % 2 === 1 && i >= 32 && i < 40) {
-  //     tile.style.background = "black";
-  //   } else if (i % 2 === 0 && i >= 40 && i < 48) {
-  //     tile.style.background = "black";
-  //   } else if (i % 2 === 1 && i >= 48 && i < 56) {
-  //     tile.style.background = "black";
-  //   } else if (i % 2 === 0 && i >= 56 && i < 64) {
-  //     tile.style.background = "black";
-  //   }
+    if (i % 2 === 1 && i < 8) {
+      tile.style.background = "black";
+    } else if (i % 2 === 0 && i >= 8 && i < 16) {
+      tile.style.background = "black";
+    } else if (i % 2 === 1 && i >= 16 && i < 24) {
+      tile.style.background = "black";
+    } else if (i % 2 === 0 && i >= 24 && i < 32) {
+      tile.style.background = "black";
+    } else if (i % 2 === 1 && i >= 32 && i < 40) {
+      tile.style.background = "black";
+    } else if (i % 2 === 0 && i >= 40 && i < 48) {
+      tile.style.background = "black";
+    } else if (i % 2 === 1 && i >= 48 && i < 56) {
+      tile.style.background = "black";
+    } else if (i % 2 === 0 && i >= 56 && i < 64) {
+      tile.style.background = "black";
+    }
 }
+=======
+>>>>>>> parent of 73fcee5 (Made a checkerboard of red and black tiles)
