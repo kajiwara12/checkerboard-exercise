fee6c79b6b3db688fa6e237b34bbee22c3b2ea2f
Quinn Kaji <quinnkaji@gmail.com>
2023-09-13 10:30:28 -1000

    changes tile color every second

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ container.style.display = "flex";
 container.style.flexWrap = "wrap";
 
 for (let i = 0; i < 64; i++) {
-  // const randomRgbColor = () => {
-  //   let r = Math.floor(Math.random() * 256); // Random between 0-255
-  //   let g = Math.floor(Math.random() * 256); // Random between 0-255
-  //   let b = Math.floor(Math.random() * 256); // Random between 0-255
-  //   return "rgb(" + r + "," + g + "," + b + ")";
-  // };
+  function randomRgbColorforBackground() {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    tile.style.background = `rgb(${r}, ${g}, ${b})`;
+  }
   const tile = document.createElement("div");
   tile.style.width = "12.5%";
   tile.style.height = "12.5%";
-  tile.style.background = "red";
+  tile.style.background = setInterval(randomRgbColorforBackground, 1000);
   container.append(tile);
 
-  if (i % 2 === 1 && i < 8) {
-    tile.style.background = "black";
-  } else if (i % 2 === 0 && i >= 8 && i < 16) {
-    tile.style.background = "black";
-  } else if (i % 2 === 1 && i >= 16 && i < 24) {
-    tile.style.background = "black";
-  } else if (i % 2 === 0 && i >= 24 && i < 32) {
-    tile.style.background = "black";
-  } else if (i % 2 === 1 && i >= 32 && i < 40) {
-    tile.style.background = "black";
-  } else if (i % 2 === 0 && i >= 40 && i < 48) {
-    tile.style.background = "black";
-  } else if (i % 2 === 1 && i >= 48 && i < 56) {
-    tile.style.background = "black";
-  } else if (i % 2 === 0 && i >= 56 && i < 64) {
-    tile.style.background = "black";
-  }
+  // if (i % 2 === 1 && i < 8) {
+  //   tile.style.background = "black";
+  // } else if (i % 2 === 0 && i >= 8 && i < 16) {
+  //   tile.style.background = "black";
+  // } else if (i % 2 === 1 && i >= 16 && i < 24) {
+  //   tile.style.background = "black";
+  // } else if (i % 2 === 0 && i >= 24 && i < 32) {
+  //   tile.style.background = "black";
+  // } else if (i % 2 === 1 && i >= 32 && i < 40) {
+  //   tile.style.background = "black";
+  // } else if (i % 2 === 0 && i >= 40 && i < 48) {
+  //   tile.style.background = "black";
+  // } else if (i % 2 === 1 && i >= 48 && i < 56) {
+  //   tile.style.background = "black";
+  // } else if (i % 2 === 0 && i >= 56 && i < 64) {
+  //   tile.style.background = "black";
+  // }
 }
